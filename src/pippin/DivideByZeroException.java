78fcf52be1cafb9b5bff5732fd6a56863bf8364d
AlexMarcus <amarcus4@binghamton.edu>
2014-12-04 20:52:22 -0500

    fixed div. by 0 class

--- src/pippin/DivideByZeroException.java
+++ src/pippin/DivideByZeroException.java
@@ -1,6 +1,11 @@
 package pippin; 
 
 public class DivideByZeroException extends RuntimeException { 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3870343547745128405L;
+
 	/* No-argument constructor needed for serialization */ 
 	public DivideByZeroException() { 
 		super(); 
