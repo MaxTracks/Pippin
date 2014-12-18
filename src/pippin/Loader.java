e56cfe2620834b0a49c9af0841475168b52610c0
Tim Hung <thung1@binghamton.edu>
2014-12-18 02:36:58 +0000

    Finished PartVII: Loader.java. I'm 95% sure it's correct, might be doing the try catch wrong though.

--- src/pippin/Loader.java
+++ src/pippin/Loader.java
@@ -18,16 +18,29 @@ public class Loader {
 			
 			//Else if in "code" read the next line as int arg and use code.setCode(inp, arg);
 			else if(inp == 1){
-				int arg = Integer.parseInt(input.nextLine());
+				try{int arg = Integer.parseInt(input.nextLine());}
+				catch (ArrayIndexOutOfBoundsException e) {
+		            JOptionPane.showMessageDialog(null, 
+		                    e.getMessage(), 
+		                    "Failure loading data", JOptionPane.WARNING_MESSAGE);
+		        }
 				code.setCode(inp, arg);
 			}
 			
 			//Else read the next line as int val and use mem.setData(inp, val);
 			else{
-				int val = Integer.parseInt(input.nextLine());
+				try{int val = Integer.parseInt(input.nextLine());}
+				catch (ArrayIndexOutOfBoundsException e) {
+		            JOptionPane.showMessageDialog(null, 
+		                    e.getMessage(), 
+		                    "Failure loading data", JOptionPane.WARNING_MESSAGE);
+		        }
 				mem.setData(inp, val);
 			}
 		}
+		
+		
+		
 		input.close();
 	}
 }
